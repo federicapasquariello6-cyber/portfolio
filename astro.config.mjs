75b166b5f70befeea21caaef12abd694c00d11a3
Federica <federica.pasquariello.6@gmail.com>
2026-06-11 13:36:50 +0200

    Aggiorna configurazione Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  adapter: vercel(),
 });
